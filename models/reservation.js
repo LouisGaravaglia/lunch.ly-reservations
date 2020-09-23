66e4694945099f15820e66435a74d0468e30a9a5
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-09-23 15:55:53 -0700

    added save() to reservations model and added fullName method for customers model

--- models/reservation.js
+++ models/reservation.js
@@ -38,6 +38,48 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  /** save this reservation. */
+  // customerId,
+  //     startAt,
+  //     numGuests,
+  //     notes
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+        [this.customerId, this.startAt, this.numGuests, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        `UPDATE reservations SET customer_id=$1, start_at=$2, num_guests=$3, notes=$4
+             WHERE id=$5`,
+        [this.customerId, this.startAt, this.numGuests, this.notes, this.id]
+      );
+    }
+  }
+
+  // async save() {
+  //   if (this.id === undefined) {
+  //     const result = await db.query(
+  //       `INSERT INTO reservations (customer_id, num_guests, start_at, notes)
+  //            VALUES ($1, $2, $3, $4)
+  //            RETURNING id`,
+  //       [this.customerId, this.numGuests, this.startAt, this.notes]
+  //     );
+  //     this.id = result.rows[0].id;
+  //   } else {
+  //     await db.query(
+  //       `UPDATE reservations SET num_guests=$1, start_at=$2, notes=$3
+  //            WHERE id=$4`,
+  //       [this.numGuests, this.startAt, this.notes, this.id]
+  //     );
+  //   }
+  // }
+
 }
 
 
